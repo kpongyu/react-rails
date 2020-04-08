0174d0087e5a439258ee549544c7480bd3a136d8
kaipengyu <Kaipeng.Yu@icf.com>
2020-04-07 23:07:34 -0400

    updating the React Components

---
+++ app/javascript/components/Article.js
@@ -0,0 +1,37 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+import Timestamp from "react-timestamp"
+
+class Article extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <div className="article-title">
+        <a href={this.props.path}>{this.props.title} </a>
+        </div>
+        <div className="article-body">
+        {this.props.description}
+        <div className="article-meta-details">
+            <small>Created by: {this.props.author}, &nbsp;
+                  <Timestamp time={this.props.created_at} precision={3}/> ago,
+                  last updated: 
+                  <Timestamp time={this.props.updated_at} precision={3}/> ago
+            </small>  
+          </div>
+        </div>
+       
+      </React.Fragment>
+    );
+  }
+}
+
+Article.propTypes = {
+  title: PropTypes.string,
+  description: PropTypes.string,
+  path: PropTypes.string,
+  author: PropTypes.string,
+  created_at: PropTypes.string,
+  updated_at: PropTypes.string
+};
+export default Article
